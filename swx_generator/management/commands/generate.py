0c300467f0e04aa81a1fbbc16243f2e1e367741d
rafaph <rafaphcastro@gmail.com>
2016-09-24 01:32:19 -0300

    Added support for newer versions of Django

--- swx_generator/management/commands/generate.py
+++ swx_generator/management/commands/generate.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals, print_function
 
-from django.core.management.base import BaseCommand, make_option, CommandError
-
+from django.core.management.base import BaseCommand, CommandError
 from swx_generator import utils
+
+try:
+    from django.core.management.base import make_option
+except ImportError:
+    pass
 
 
 class Command(BaseCommand):
@@ -12,22 +16,48 @@ class Command(BaseCommand):
     Create a view:\n
         python manage.py generate <app_name> --view=<view_name>
     """
-    option_list = BaseCommand.option_list + (
-        make_option(
-            "--model",
-            dest="model",
-            help="specify the django model name",
-            metavar="MODEL",
+
+    def __init__(self, *args, **kwargs):
+        if hasattr(self, 'option_list'):
+            self.option_list = BaseCommand.option_list + (
+                make_option(
+                    '--model',
+                    dest='model',
+                    help='specify the django model name',
+                    metavar='MODEL',
+                    default=None
+                ),
+                make_option(
+                    '--view',
+                    dest='view',
+                    help='specify the django view file name',
+                    metavar='VIEW',
+                    default=None
+                ),
+            )
+        super(Command, self).__init__(*args, **kwargs)
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'args',
+            metavar='APPNAME',
+            nargs='*',
+            help='specify app name',
+        )
+        parser.add_argument(
+            '--model',
+            dest='model',
+            help='specify the django model name',
+            metavar='MODEL',
             default=None
-        ),
-        make_option(
-            "--view",
-            dest="view",
-            help="specify the django view file name",
-            metavar="VIEW",
+        )
+        parser.add_argument(
+            '--view',
+            dest='view',
+            help='specify the django view file name',
+            metavar='VIEW',
             default=None
-        ),
-    )
+        )
 
     @staticmethod
     def __validate_input(*args, **kwargs):
